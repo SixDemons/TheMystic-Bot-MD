be28be89d47d71265ba91cca6c082abf7ac25416
SixDemons <118392641+SixDemons@users.noreply.github.com>
2022-11-16 15:09:34 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,14 +8,14 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['Unptoadrih15', '👑 Mystic - Collaborator 1 👑', true],
-  ['50499698072', '👑 Mystic - Collaborator 2 👑', true],
-  ['51940617554', '👑 Mystic - Collaborator 3 👑', true],
-  ['51996089079', '👑 Mystic - Collaborator 4 👑', true],
-  ['573225236629', '👑 Mystic - Collaborator 5 👑', true],
-  ['593959425714', '👑 Mystic - Collaborator 6 👑', true],
-  ['50246028932', '👑 Mystic - Collaborator 7 👑', true],  
+  ['51977802958', '👑 BarrioKing - Creador 👑', true],
+  ['Unptoadrih15', '👑 BarrioKing - Collaborator 1 👑', true],
+  ['50499698072', '👑 BarrioKing - Collaborator 2 👑', true],
+  ['51940617554', '👑 BarrioKing - Collaborator 3 👑', true],
+  ['51996089079', '👑 BarrioKing - Collaborator 4 👑', true],
+  ['573225236629', '👑 BarrioKing - Collaborator 5 👑', true],
+  ['593959425714', '👑 BarrioKing - Collaborator 6 👑', true],
+  ['50246028932', '👑 BarrioKing - Collaborator 7 👑', true],  
   ['593968585283'],
   ['5219992095479'],
   ['5219991402134'],
@@ -27,9 +27,9 @@ global.suittag = ['5219993404349']
 global.prems = ['51995386439'] 
 
 global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.wm = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.igfg = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
+global.author = '★BarrioKing - 𝙱𝚘𝚝★'
+global.wm = '★BarrioKing - 𝙱𝚘𝚝★'
+global.igfg = '★BarrioKing - 𝙱𝚘𝚝★'
 global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
 
 global.keysZens = ['fiktod', 'c2459db922', 'BF39D349845E', '675e34de8a', '37CC845916', '0b917b905e6f', '6fb0eff124']
